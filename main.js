196cca086a4ccbb558424b6036445426ccbb1e51
diaz farindra <farindrad@gmail.com>
2020-02-06 16:05:17 +0700

    try again

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
-const signInButton = document.getElementById('signInButton')
-const signOutButton = document.getElementById('signOutButton')
-
 function onSignIn(googleUser) {
-    const profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-onSignIn(googleuser)
 
-function updateStatus() {
+    const signInButton = document.getElementById('signInButton')
+    const signOutButton = document.getElementById('signOutButton')
 
-    if (googleUser === true) {
+    if (onSignIn()) {
         signInButton.style.display = 'none'
         signOutButton.style.display = 'block'
     } else {
@@ -20,5 +11,10 @@ function updateStatus() {
         signOutButton.style.display = 'none'
     }
 
+    const profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
-updateStatus()
+onSignIn()
