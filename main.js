fe25cc39dc37fdb4439ab6b775ff130f5a43242b
diaz farindra <farindrad@gmail.com>
2020-02-06 14:10:27 +0700

    new JS display and block to show and hide button

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const signInButton = document.getElementById('signInButton')
+const signOutButton = document.getElementById('signOutButton')
+
 function onSignIn(googleUser) {
     const profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
@@ -5,5 +8,16 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
-
 onSignIn()
+
+function updateStatus(onSignIn) {
+
+    if (onSignIn === true) {
+        signInButton.style.display = 'none'
+        signOutButton.style.display = 'block'
+    } else {
+        signInButton.style.display = 'block'
+        signOutButton.style.display = 'none'
+    }
+
+}
