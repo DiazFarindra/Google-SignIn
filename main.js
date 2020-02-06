065ab19a7326ff58a62058042b8a01c5ba4a5519
diaz farindra <farindrad@gmail.com>
2020-02-06 16:47:14 +0700

    yeah

--- main.js
+++ main.js
@@ -3,13 +3,8 @@ function onSignIn(googleUser) {
     const signInButton = document.getElementById('signInButton')
     const signOutButton = document.getElementById('signOutButton')
 
-    if (onclick(onSignIn())) {
-        signInButton.style.display = 'none'
-        signOutButton.style.display = 'block'
-    } else {
-        signInButton.style.display = 'block'
-        signOutButton.style.display = 'none'
-    }
+    signInButton.style.display = 'none'
+    signOutButton.style.display = 'block'
 
     const profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
